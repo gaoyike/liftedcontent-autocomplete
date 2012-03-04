0ed4ce22ec9f55b6495eb35182c325a347559a21
Stefan Bradl <stefan_bradl@web.de>
2012-03-04 17:48:28 +0100

    added scala doc

--- src/main/scala/eu/sbradl/autocomplete/AutoComplete.scala
+++ src/main/scala/eu/sbradl/autocomplete/AutoComplete.scala
@@ -9,22 +9,50 @@ import net.liftweb.util.Helpers.nextFuncName
 
 object AutoComplete {
 
+  /**
+   * Creates a new auto-complete object.
+   * 
+   * @param choices The function used to get key-value pairs matching the given input.
+   * @param minChars The minimum number of characters needed to trigger auto-completion.
+   */
   def apply(choices: (String) => Seq[(String, String)], minChars: Int = 3) = new AutoComplete(choices, minChars)
 
 }
 
+/**
+ * This class makes HTML5 autocompletion very easy to use. The fastest way to get
+ * autocompletion to work is by using the methods [[AutoComplete.datalist]] and 
+ * [[AutoComplete.input]]. If you need to use an already existing input element you 
+ * can use [[AutoComplete.onChange]] to trigger the autocompletion.
+ */
 class AutoComplete(choices: (String) => Seq[(String, String)], minChars: Int) {
 
+  /**
+   * The ID used for the datalist element.
+   */
   val datalistId = nextFuncName
+  
+  /**
+   * The name used for the input element.
+   */
   val inputName = nextFuncName
 
+  /**
+   * Returns an initial datalist element.
+   */
   def datalist: NodeSeq = datalist("")
 
-  def input = <input type="text" name={ inputName } list={ datalistId } onchange={ onChange._2.toJsCmd }/>
+  /**
+   * Returns a ready to use input element with auto-completion.
+   */
+  def input = <input type="text" name={ inputName } list={ datalistId } oninput={ onChange._2.toJsCmd }/>
 
+  /**
+   * Returns an ajax function to be used with custom input elements.
+   */
   def onChange = SHtml.onEvent(updateDatalist _)
 
-  private def datalist(current: String): NodeSeq = <datalist id={ datalistId }>
+  private[autocomplete] def datalist(current: String): NodeSeq = <datalist id={ datalistId }>
                                                      { datalistContent(current) }
                                                    </datalist>
 
